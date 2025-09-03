febbb11f53e0806997131b13c4cc673b0f0d0b18
wei939 <80055479+wei939@users.noreply.github.com>
2025-09-03 16:32:00 +0800

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+function setCookie(name, value, days) {
+    // 使用 escape 函数对 cookie 的值进行编码
+    var encodedValue = escape(value);
+    
+    // 创建一个日期对象
+    var date = new Date();
+    
+    // 设置 cookie 的过期时间
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    
+    // 获取过期时间的字符串表示
+    var expires = "expires=" + date.toUTCString();
+    
+    // 设置 cookie
+    document.cookie = name + "=" + encodedValue + ";" + expires + ";path=/";
+}
